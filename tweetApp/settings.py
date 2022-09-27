9d0fd2d1f8e9f539d0a73a6eb0b66ea771dd3f1c
Divyasha <divyasha@techis.io>
2022-09-27 10:45:14 +0530

    changes done

--- tweetApp/settings.py
+++ tweetApp/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.milddleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
